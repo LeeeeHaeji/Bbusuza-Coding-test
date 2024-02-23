4210542d7ec2c3c0440dd81de1154db53d36b78d
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2024-02-23 19:30:09 +0900

    Update 폰켓몬_haeji.js

--- [Lv.1]/폰켓몬/폰켓몬_haeji.js
+++ [Lv.1]/폰켓몬/폰켓몬_haeji.js
@@ -1,1 +1,15 @@
+function solution(nums) {
+    var answer = 0;
+    let num = nums.length/2;
+    let choose = new Set(nums);
+    
+    if(num > choose.size){
+        // 선택하는 횟수가 종의 수보다 많다면
+        answer = choose.size;
+    } else{
+        // 선택하는 횟수가 종의 수보다 적다면
+        answer = num;
+    }
 
+    return answer;
+}
