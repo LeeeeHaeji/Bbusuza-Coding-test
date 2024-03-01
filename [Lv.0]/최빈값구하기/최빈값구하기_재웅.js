98afff6e9395a0d9eaf17b29240db9a259b5b853
AYFG <88381607+AYFG@users.noreply.github.com>
2024-03-01 22:07:47 +0900

    Update 최빈값구하기_재웅.js

--- [Lv.0]/최빈값구하기/최빈값구하기_재웅.js
+++ [Lv.0]/최빈값구하기/최빈값구하기_재웅.js
@@ -1,1 +1,18 @@
-
+function solution(array) {
+    let result = 0;
+    let obj = {};
+      array.map((value)=>{
+        if(Object.keys(obj).includes(value.toString())){
+            obj[value] = obj[value] + 1;
+        }else{
+            obj[value] = 1;
+        }
+      })
+    const values = Object.values(obj).sort((a,b)=>b-a)
+    
+    if(values[0]!==values[1]){
+          return parseInt(Object.keys(obj).find((key)=>obj[key]===values[0]))
+    }else{
+         return result = -1
+    }
+}
