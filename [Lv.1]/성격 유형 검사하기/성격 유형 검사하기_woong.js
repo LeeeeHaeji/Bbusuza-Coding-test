6c946e090ec5579ed8ff30c422a0c8a4bfd530a7
AYFG <88381607+AYFG@users.noreply.github.com>
2024-03-20 20:30:17 +0900

    Update 성격 유형 검사하기_woong.js

--- [Lv.1]/성격 유형 검사하기/성격 유형 검사하기_woong.js
+++ [Lv.1]/성격 유형 검사하기/성격 유형 검사하기_woong.js
@@ -66,3 +66,52 @@ function solution(survey, choices) {
     
     return answer
 }
+
+function solution(survey, choices) {
+    var answer = '';
+    const obj = {
+    R: 0,
+    T: 0,
+    C: 0,
+    F: 0,
+    J: 0,
+    M: 0,
+    A: 0,
+    N: 0,
+  };
+    for(let i = 0; i <survey.length; i++){
+        if(choices[i] === 4){
+            continue;
+        }        
+        if(choices[i] < 4){
+            obj[survey[i][0]] += 4 - choices[i]
+        }
+        if(choices[i] > 4){
+            obj[survey[i][1]] += choices[i] - 4
+        }
+        
+    }
+    
+    if(obj["R"] >= obj["T"]){
+        answer += "R"
+    }else{
+        answer += "T"
+    }
+    if(obj["C"] >= obj["F"]){
+        answer += "C"
+    }else{
+        answer += "F"
+    }
+    if(obj["J"] >= obj["M"]){
+        answer += "J"
+    }else{
+        answer += "M"
+    }
+    if(obj["A"] >= obj["N"]){
+        answer += "A"
+    }else{
+        answer += "N"
+    }
+    
+    return answer
+}
