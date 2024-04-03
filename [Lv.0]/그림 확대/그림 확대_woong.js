374e15c4688fd7407f760ca707abaffdb6eb7763
AYFG <88381607+AYFG@users.noreply.github.com>
2024-04-03 15:12:37 +0900

    Update 그림 확대_woong.js

--- [Lv.0]/그림 확대/그림 확대_woong.js
+++ [Lv.0]/그림 확대/그림 확대_woong.js
@@ -1,1 +1,10 @@
-
+function solution(picture, k) {
+    var answer = [];
+    picture.map((v)=>{
+        const arr = [...v].map((v)=> v.repeat(k)).join("")
+        for(let i = 0; i < k; i++){
+        answer.push(arr)    
+        }
+    })
+    return answer;
+}
