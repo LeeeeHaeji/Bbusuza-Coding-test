c933031feeaf04068b86a11f992315e5b8af2210
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-04-28 14:33:10 +0900

    K번째수_sooyeon

---
+++ [Lv.1]/K번째수/K번째수_sooyeon.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    let answer = [];
+    for(let i = 0; i<commands.length; i++){
+        let arr = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=> a - b)
+        answer.push(arr[commands[i][2]-1])
+    }
+    return answer;
+}
