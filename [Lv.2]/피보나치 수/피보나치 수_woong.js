953fde0c4dcbbc0f46a3e391610dac418bea5176
AYFG <88381607+AYFG@users.noreply.github.com>
2024-05-29 20:32:40 +0900

    Update 피보나치 수_woong.js

--- [Lv.2]/피보나치 수/피보나치 수_woong.js
+++ [Lv.2]/피보나치 수/피보나치 수_woong.js
@@ -20,6 +20,21 @@
 //     }
 //     return recursion(n)
 // }
+// function solution(n){
+//     let answer = 0;
+//     let temp0 = 0;
+//     let temp1 = 1;
+//     const remainder = 1234567
+    
+//     for(let i = 2; i <= n; i++){
+//         // https://school.programmers.co.kr/learn/courses/14743/lessons/116435
+//         // answer = ((temp0 % remainder) + (temp1 % remainder) % remainder); // 괄호를 틀렸음. remainder가 (temp1 % remainder)의 나머지를 구하고 있음
+//         answer = (temp0 +temp1) % remainder;
+//         temp0 = temp1;
+//         temp1 = answer;
+//     }
+//     return answer 
+// }
 function solution(n){
     let answer = 0;
     let temp0 = 0;
@@ -27,9 +42,7 @@ function solution(n){
     const remainder = 1234567
     
     for(let i = 2; i <= n; i++){
-        // https://school.programmers.co.kr/learn/courses/14743/lessons/116435
-        // answer = ((temp0 % remainder) + (temp1 % remainder) % remainder); 
-        answer = (temp0 +temp1) % remainder;
+        answer = ((temp0 % remainder) + (temp1 % remainder)) % remainder;
         temp0 = temp1;
         temp1 = answer;
     }
