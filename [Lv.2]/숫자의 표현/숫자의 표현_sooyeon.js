9647a826c7970f73b64802e3b6a12734a3d9c5ad
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2025-02-06 19:18:07 +0900

    Create 숫자의 표현_sooyeon.js

---
+++ [Lv.2]/숫자의 표현/숫자의 표현_sooyeon.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    var answer = 0
+
+    for(let i = 1; i <= n; i++)
+    {
+        let sum = 0
+        for(let j = i; j <=n; j++)
+        {
+            sum += j;
+            if(sum > n) break;
+            if(sum === n) {
+                ++answer;
+                break;}
+            }
+        }
+    return answer;
+}
