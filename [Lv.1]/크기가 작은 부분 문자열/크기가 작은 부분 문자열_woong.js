7b503c4d681761a9a1610be5173f5f22d24f57ac
AYFG <sek3137@naver.com>
2025-03-29 14:44:29 +0900

    크기가 작은 부분 문자열_woong.js 만들기

---
+++ [Lv.1]/크기가 작은 부분 문자열/크기가 작은 부분 문자열_woong.js
@@ -0,0 +1,14 @@
+function solution(t, p) {
+    var answer = 0;
+    let arr = [];for(let i = 0; i < t.length; i++){
+//         t의 첫 인덱스(i)부터 p의 길이만큼 잘라서 p와 비교 And t의 첫 인덱스부터 p의 길이만큼 잘랐을 때 p와 길이가 같은지
+        if(t.slice(i,i + p.length) <= p && t.slice(i,i + p.length).length == p.length){
+           answer++;
+           console.log(t.slice(i,i + p.length))
+         }
+    }
+    
+    
+   
+    return answer;
+}
